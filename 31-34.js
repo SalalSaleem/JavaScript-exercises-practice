c47767f83cf87a30a071e1586aeb892cbe20ea4c
Salal Saleem <salalsaleem2420@gmail.com>
2024-11-15 20:36:38 +0500

    updated

---
+++ 31-34.js
@@ -0,0 +1,65 @@
+//// chapter 31 ////
+
+// 01 //
+
+// console.log(new Date())
+
+// 02 //
+
+// var a = new Date().getMonth();
+// var monthsList = ["jan","feb","march","april","may","jun","july","aug","sept","oct","nov","dec"]
+
+// console.log(monthsList[a]);
+
+// 03 //
+
+// var a = new Date().getDay();
+// var daysList = ["sun","mon","tues","wed","thurs","fri","sat"]
+
+// console.log(daysList[a]);
+
+
+// 04 //
+
+// var a = new Date().getDay();
+// var daysList = ["sun","mon","tues","wed","thurs","fri","sat"]
+
+// if(daysList[a] == "sun" || daysList[a] == "sat" ){
+//     console.log("Its Fun day Today !");
+// }else{
+//     console.log("Its not Fun day Today !");
+// }
+
+// 05 //
+
+// var a = new Date().getDate();
+
+// if(a < 16){
+//     console.log("First fifteen days of the month");
+// }else{
+//     console.log("last days of the month");
+// }
+
+// 06 //
+
+// var date = new Date();
+
+// var mili = new Date().getTime();
+
+// var mins = date / 1000 / 60    
+
+// document.write("Current date : " + date + "<br>" + "Elapsed Miliseconds since January 1,1970 : " + mili + "<br>" +"Elapsed minutes since January 1,1970 : " + mins )
+
+
+// 07 //
+
+// var hours = new Date().getHours();
+
+// if( hours > 12){
+//     console.log("Its PM")
+// }else{
+//     console.log("Its Am");
+// }
+
+// 09 //
+
