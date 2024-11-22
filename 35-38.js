7d0fa3f548df0ccbabbe0f5cbae3be3525e5a3c0
Salal Saleem <salalsaleem2420@gmail.com>
2024-11-22 20:10:08 +0500

    updated

---
+++ 35-38.js
@@ -0,0 +1,114 @@
+//// chapter 31 ////
+
+// 01 //
+
+// function dateAndTime(){
+//     var date = new Date();
+//     console.log(date);
+    
+// }
+
+// dateAndTime()
+
+
+// 02 //
+
+// function greeting(){
+//     var first = prompt("what is your first name?");
+//     var last = prompt("what is your last name?");
+
+//     document.write("welcome to our site " + first+" "+last)
+// }
+
+// greeting();
+
+
+// 03 //
+
+// function sum(){
+//     var firstNum = +prompt("first number");
+//     var secondNum = +prompt("second number");
+//     var result = firstNum+secondNum;
+//     document.write("The result of " + firstNum+" + "+secondNum + " is " + result)
+// }
+
+// sum();
+
+
+// 04 //
+
+
+// function calculator(){
+//     var firstNum = +prompt("enter first number");
+//     var operation = prompt("enter operator");
+//     var lastNum = +prompt("enter last number");
+//     if(operation === "+"){
+//         document.write(firstNum+lastNum)
+//     }else if(operation=== "-"){
+//         document.write(firstNum-lastNum)
+//     }
+//     else if(operation=== "*"){
+//         document.write(firstNum*lastNum)
+//     }
+//     else if(operation=== "/"){
+//         document.write(firstNum/lastNum)
+//     }
+// }
+
+// calculator()
+
+
+// 05 //
+
+// var userInput = +prompt("enter a number");
+
+// function square(userInput){
+//     document.write(userInput*userInput);    
+// }
+
+// square(userInput)
+
+// 06 //
+
+// var numb = +prompt("enter a number");
+
+// function factorial(numb){
+//     for(var i = numb-1 ; i > 0 ; i--){
+//        var result = numb*i;
+//         numb = result;
+//     }
+//     document.write(numb)
+// }
+
+// factorial(numb)
+
+
+// 07 //
+
+// var firstNum = +prompt("first number?");
+// var lastNum = +prompt("last number?");
+
+// function counter(firstNum,lastNum){
+//     for(var i = firstNum ; i <= lastNum ; i++){
+//         document.write(i + "<br/>")
+//     }
+// }
+
+// counter(firstNum,lastNum)
+
+// 08 //
+
+
+// 09 //
+
+// var height = +prompt("what is the height of a rectangle?")
+// var width = +prompt("what is the width of a rectangle?")
+
+// function area(height,width){
+//     var result = height+width;
+//     document.write("the area of rectangle is " + result)
+// }
+
+// area(height,width)
+
+
